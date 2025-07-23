b3d9e6fefdc7af9b459b6d0e91d982b950b637a7
DELTETTAT <e38479905@gmail.com>
2025-07-23 14:40:14 +0530

    Delete face_matching_system - Copy directory

--- face_matching_system - Copy/src/interfaces/cli/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Command-line interface tools for face matching system.
-
-Provides various CLI utilities:
-- search: Face similarity search
-- build_index: FAISS index building
-- preprocess: Image preprocessing
-- orchestrator: Interactive menu system
-"""
--- face_matching_system - Copy/src/interfaces/web/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Web interface module for face matching system.
-
-Provides Streamlit web application with multiple pages:
-- app: Main application entry point
-- pages: Individual application pages
-"""
