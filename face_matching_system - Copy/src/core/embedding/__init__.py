3c08abfdd3b2bd424375c053ca9c51a1caacb3ee
DELTETTAT <e38479905@gmail.com>
2025-07-23 15:42:47 +0530

    Delete face_matching_system - Copy/src directory

--- face_matching_system - Copy/src/core/embedding/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Embedding extraction module for face matching system.
-
-Provides face embedding extraction using DeepFace models.
-"""
-
-from .extractor import EmbeddingExtractor
-
-__all__ = ['EmbeddingExtractor']
--- face_matching_system - Copy/src/interfaces/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Interface modules for face matching system.
-
-Provides both CLI and web interfaces:
-- cli: Command-line interface tools
-- web: Streamlit web application
-"""
