7cfdc9b841d5eb473215f813053484229f481e95
DELTETTAT <e38479905@gmail.com>
2025-07-24 16:10:48 +0530

    Delete face_matching_system - Copy directory

--- face_matching_system - Copy/src/core/indexing/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-FAISS index management module for face matching system.
-
-Provides FAISS index creation, loading, and management capabilities.
-"""
-
-from .manager import IndexManager
-
-__all__ = ['IndexManager']
