8c2fec7a2add9be572ce36056e482c7492379398
DELTETTAT <e38479905@gmail.com>
2025-07-24 12:55:58 +0530

    Delete face_matching_system - Copy directory

--- face_matching_system - Copy/src/core/search/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Similarity search engine module for face matching system.
-
-Provides FAISS-based similarity search capabilities.
-"""
-
-from .engine import SearchEngine
-
-__all__ = ['SearchEngine']
